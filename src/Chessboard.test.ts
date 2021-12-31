48b3792032b0322d57e3dd6862b54f7f8d0f9e2d
Milind Ganjoo <milind.ganjoo@gmail.com>
2021-12-30 20:22:32 -0800

    Consolidate styles and Chessboard.ts

--- src/Chessboard.test.ts
+++ src/Chessboard.test.ts
@@ -1,5 +1,89 @@
+import * as fc from "fast-check"
+import {
+  getOppositeSide,
+  getSquare,
+  getSquareColor,
+  getVisualRowColumn,
+  Piece,
+  Side,
+  Square,
+} from "./utils-chess"
+import { waitFor, within } from "@testing-library/dom"
+import userEvent from "@testing-library/user-event"
+import { Chessboard } from "./Chessboard"
+
+function buildChessboard(
+  orientation: Side,
+  pieces?: Partial<Record<Square, Piece>>
+): [HTMLElement, Chessboard] {
+  const wrapper = document.createElement("div")
+  return [wrapper, new Chessboard(wrapper, { orientation, pieces })]
+}
+
+describe.each<{ side: Side; flip: boolean }>([
+  { side: "white", flip: false },
+  { side: "white", flip: true },
+  { side: "black", flip: false },
+  { side: "black", flip: true },
+])(
+  "Chessboard with orientation = $side, flip orientation after creation = $flip",
+  ({ side, flip }) => {
+    const pieces = {
+      b3: { color: "white", pieceType: "queen" },
+      e6: { color: "black", pieceType: "knight" },
+      g1: { color: "white", pieceType: "king" },
+    } as const
+    const [wrapper, chessboard] = buildChessboard(side, pieces)
+    if (flip) {
+      chessboard.updateOrientationAndRedraw(getOppositeSide(side))
+    }
+    const finalSide = flip ? getOppositeSide(side) : side
+    const idxsWithPieces = (Object.keys(pieces) as (keyof typeof pieces)[])
+      .map((s) => getVisualRowColumn(s, finalSide))
+      .flatMap(([row, col]) => row * 8 + col)
+
+    it("should add correct classes and attributes to chessboard", () => {
+      fc.assert(
+        fc.property(fc.nat({ max: 7 }), fc.nat({ max: 7 }), (row, col) => {
+          const squareElement =
+            wrapper.querySelectorAll(".square")[row * 8 + col]
+          const expectedSquare = getSquare(row, col, finalSide)
+          expect(squareElement).toHaveAttribute("data-square", expectedSquare)
+          expect(squareElement).toHaveClass(getSquareColor(expectedSquare))
+        })
+      )
+    })
+
+    it("should not have the .has-piece class on chessboard without pieces", () => {
+      fc.assert(
+        fc.property(
+          fc.nat({ max: 63 }).filter((idx) => !idxsWithPieces.includes(idx)),
+          (idx) => {
+            expect(wrapper.querySelectorAll(".square")[idx]).not.toHaveClass(
+              "has-piece"
+            )
+          }
+        )
+      )
+    })
+
+    it("should have the .has-piece class on chessboard with pieces", () => {
+      idxsWithPieces.forEach((i) => {
+        expect(wrapper.querySelectorAll(".square")[i]).toHaveClass("has-piece")
+      })
+    })
+  }
+)
+
 describe("Chessboard", () => {
-  it("passes simple test", () => {
-    expect(2 + 2).toBe(4)
+  it.skip("should correctly handle two-click moves", async () => {
+    const [wrapper] = buildChessboard("white", {
+      c3: { color: "black", pieceType: "rook" },
+    })
+    userEvent.click(within(wrapper).getByRole("button", { name: /c3/i }))
+    await waitFor(() =>
+      expect(wrapper.firstElementChild).toHaveClass("awaiting-second-touch")
+    )
+    userEvent.click(within(wrapper).getByLabelText(/e7/i))
   })
 })
--- src/Chessboard.ts
+++ src/Chessboard.ts
@@ -1,14 +1,49 @@
+import {
+  getOppositeSquareColor,
+  getSquare,
+  getSquareColor,
+  getVisualRowColumn,
+  keyIsSquare,
+  Piece,
+  Side,
+  Square,
+} from "./utils-chess"
+import { Pieces } from "./Pieces"
+import { makeHTMLElement, removeElement } from "./utils-dom"
+import { assertUnreachable } from "./utils-typing"
 import "./styles.css"
-import { Squares } from "./Squares"
-import { Piece, Side, Square } from "./utils-chess"
 
 export interface ChessboardConfig {
   orientation?: Side
   pieces?: Partial<Record<Square, Piece>>
 }
 
+interface AwaitingInput {
+  id: "awaiting-input"
+}
+interface ClickingFirstSquare {
+  id: "touching-first-square"
+  startSquare: Square
+}
+
+interface AwaitingSecondClick {
+  id: "awaiting-second-touch"
+  startSquare: Square
+}
+
+type MoveState = AwaitingInput | ClickingFirstSquare | AwaitingSecondClick
+
+const HAS_PIECE_CLASS = "has-piece"
 export class Chessboard {
-  private squares: Squares
+  private group: HTMLDivElement
+  private squareElements: HTMLDivElement[][]
+  private pieces: Pieces
+  private moveState: MoveState
+  private orientation: Side
+
+  // Event handlers
+  private mouseDownHandler: (e: MouseEvent) => void
+  private mouseUpHandler: (e: MouseEvent) => void
 
   /**
    * Creates a Chessboard UI element and appends it to `container`.
@@ -17,18 +52,164 @@ export class Chessboard {
    *                  Rendered chessboard will be appended to this container.
    * @param config Configuration for chessboard (see type definition for details)
    */
-  constructor(container: HTMLElement, config: ChessboardConfig) {
-    this.squares = new Squares(
-      container,
-      config.orientation || "white",
-      config.pieces
-    )
+  constructor(container: Element, config: ChessboardConfig) {
+    this.squareElements = new Array(8)
+    this.moveState = { id: "awaiting-input" }
+    this.orientation = config.orientation || "white"
+
+    this.group = makeHTMLElement("div", { attributes: { role: "grid" } })
+    for (let i = 0; i < 8; i++) {
+      this.squareElements[i] = new Array(8)
+      const row = makeHTMLElement("div", {
+        attributes: { role: "row" },
+      })
+      for (let j = 0; j < 8; j++) {
+        this.squareElements[i][j] = makeHTMLElement("div", {
+          classes: ["square"],
+        })
+        row.appendChild(this.squareElements[i][j])
+      }
+      this.group.appendChild(row)
+    }
+    container.appendChild(this.group)
+
+    // Build pieces
+    this.pieces = new Pieces(this.group, this.orientation, config.pieces)
+
+    // Initial render
+    this.updateClasses()
+    this.draw()
+
+    // Add listeners
+    this.mouseDownHandler = this.handleMouseDown.bind(this)
+    this.group.addEventListener("mousedown", this.mouseDownHandler)
+    this.mouseUpHandler = this.handleMouseUp.bind(this)
+    this.group.addEventListener("mouseup", this.mouseUpHandler)
+  }
+
+  cleanup() {
+    removeElement(this.group)
+    this.group.removeEventListener("mousedown", this.mouseDownHandler)
+    this.group.removeEventListener("mouseup", this.mouseUpHandler)
+  }
+
+  updateOrientationAndRedraw(orientation: Side) {
+    this.orientation = orientation
+    this.pieces.updateOrientationAndRedraw(orientation)
+    this.draw()
+  }
+
+  private draw() {
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        const square = getSquare(i, j, this.orientation)
+        const color = getSquareColor(square)
+        this.squareElements[i][j].classList.remove(
+          getOppositeSquareColor(color)
+        )
+        this.squareElements[i][j].classList.add(color)
+        this.squareElements[i][j].dataset.square = square
+        this.updatePieceContained(square)
+
+        // Rank labels
+        if (j === 0) {
+          this.squareElements[i][j].dataset.rankLabel = `${
+            this.orientation === "white" ? 8 - i : i + 1
+          }`
+        }
+
+        // File labels
+        if (i === 7) {
+          this.squareElements[i][j].dataset.fileLabel = String.fromCharCode(
+            "a".charCodeAt(0) + (this.orientation === "white" ? j : 7 - j)
+          )
+        }
+      }
+    }
+  }
+
+  private movePiece(from: Square, to: Square) {
+    if (this.pieces.movePiece(from, to)) {
+      this.updatePieceContained(from)
+      this.updatePieceContained(to)
+    }
+  }
+
+  private handleMouseDown(e: MouseEvent) {
+    const target = e.target as HTMLElement
+    const clickedSquare = target.dataset.square
+
+    if (keyIsSquare(clickedSquare)) {
+      switch (this.moveState.id) {
+        case "awaiting-input":
+          if (this.pieces.hasPieceOn(clickedSquare)) {
+            this.moveState = {
+              id: "touching-first-square",
+              startSquare: clickedSquare,
+            }
+          }
+          break
+        case "touching-first-square":
+          // We are in the middle of touching first square (which may turn into
+          // drag). Browser shouldn't have fired this event again.
+          console.warn(`Unexpected event: ${e}`)
+          break
+        case "awaiting-second-touch":
+          this.movePiece(this.moveState.startSquare, clickedSquare as Square)
+          this.moveState = { id: "awaiting-input" }
+          break
+        default:
+          assertUnreachable(this.moveState)
+      }
+
+      this.updateClasses()
+    }
+  }
+
+  private handleMouseUp(e: MouseEvent) {
+    const target = e.target as HTMLElement
+    const clickedSquare = target.dataset.square
+
+    if (keyIsSquare(clickedSquare)) {
+      switch (this.moveState.id) {
+        case "touching-first-square":
+          this.moveState = {
+            id: "awaiting-second-touch",
+            startSquare: this.moveState.startSquare,
+          }
+          break
+      }
+    }
+
+    this.updateClasses()
   }
 
   /**
-   * Remove chessboard from DOM, and de-register all event handlers.
+   * Update classes for squares group as well as any
+   * highlighted/selected squares.
    */
-  cleanup() {
-    this.squares.cleanup()
+  private updateClasses() {
+    this.group.classList.value = `chessboard ${this.moveState.id}`
   }
+
+  /**
+   * Update UI elements associated with a given square, depending
+   * on whether it contains a piece.
+   */
+  private updatePieceContained(square: Square) {
+    const hasPiece = this.pieces.hasPieceOn(square)
+    const [row, column] = getVisualRowColumn(square, this.orientation)
+    this.squareElements[row][column].classList.toggle(HAS_PIECE_CLASS, hasPiece)
+    if (hasPiece) {
+      this.squareElements[row][column].setAttribute("role", "gridcell")
+      this.squareElements[row][column].setAttribute("aria-label", square)
+      this.squareElements[row][column].tabIndex = 0
+    } else {
+      this.squareElements[row][column].removeAttribute("role")
+      this.squareElements[row][column].removeAttribute("aria-label")
+      this.squareElements[row][column].removeAttribute("tabindex")
+    }
+  }
+
+  // TODO: mousemove
 }
