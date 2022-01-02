cb502bbe4948b940b3c57c9cc06ff784713a6e41
Milind Ganjoo <milind.ganjoo@gmail.com>
2022-01-02 11:57:37 -0800

    Remove default stories

--- src/Chessboard.stories.ts
+++ src/Chessboard.stories.ts
@@ -1,47 +1,57 @@
 import { Meta, Story } from "@storybook/html"
-import { Chessboard } from "./Chessboard"
+import { Chessboard, ChessboardConfig } from "./Chessboard"
 import { Side } from "./utils/chess"
-interface ChessboardProps {
-  orientation: Side
-  showOutsideLink: boolean
-}
 
+// Assign as variable to ensure type checking
 const SIDE_OPTIONS: Side[] = ["white", "black"]
 
 export default {
+  title: "Chessboard",
+  decorators: [
+    (storyDiv) => {
+      const d = storyDiv() as HTMLDivElement
+      d.style.maxWidth = "24rem"
+      return d
+    },
+  ],
   argTypes: {
     orientation: {
       options: SIDE_OPTIONS,
       control: { type: "radio" },
     },
-    showOutsideLink: {
-      type: "boolean",
-    },
   },
 } as Meta
 
-const Template: Story<ChessboardProps> = ({ orientation, showOutsideLink }) => {
-  const wrapperDiv = document.createElement("div")
-  wrapperDiv.style.maxWidth = "28rem"
-  new Chessboard(wrapperDiv, {
-    orientation: orientation,
-    pieces: {
-      e4: { color: "white", pieceType: "queen" },
-      g5: { color: "black", pieceType: "knight" },
-      b2: { color: "white", pieceType: "king" },
-    },
-  })
-  if (showOutsideLink) {
-    const a = document.createElement("a")
-    a.href = "#"
-    a.innerText = "sample outside link to test focus/blur"
-    wrapperDiv.appendChild(a)
-  }
-  return wrapperDiv
+const Template: Story<ChessboardConfig> = (config) => {
+  const container = document.createElement("div")
+  new Chessboard(container, config)
+  return container
 }
 
 export const Default = Template.bind({})
 Default.args = {
   orientation: "white",
-  showOutsideLink: false,
+  pieces: {
+    e4: { color: "white", pieceType: "queen" },
+    g5: { color: "black", pieceType: "knight" },
+    b2: { color: "white", pieceType: "king" },
+  },
 }
+
+export const Wrapped = Template.bind({})
+Wrapped.args = {
+  ...Default.args,
+}
+Wrapped.decorators = [
+  (storyDiv) => {
+    const wrapper = document.createElement("div")
+    wrapper.appendChild(storyDiv() as HTMLDivElement)
+    wrapper.insertAdjacentHTML(
+      "beforeend",
+      `<p>
+        Here is some sample text, including a <a href="#">link</a>, to test focus/blur.
+      </p>`
+    )
+    return wrapper
+  },
+]
