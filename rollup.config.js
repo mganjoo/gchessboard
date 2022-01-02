1ab56fb1e9dab893a3fd0cb167147348be626c90
Milind Ganjoo <milind.ganjoo@gmail.com>
2022-01-02 12:02:55 -0800

    Exclude files in the stories/ subdirectory from building

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,12 @@ import pkg from "./package.json"
 /** @type {import("@rollup/plugin-typescript").RollupTypescriptOptions } */
 const typescriptConfig = {
   tsconfig: "./tsconfig.json",
-  exclude: ["src/**/*.test.(ts|tsx)", "src/**/*.stories.(ts|tsx)", "./*.ts"],
+  exclude: [
+    "src/**/*.test.(ts|tsx)",
+    "src/**/*.stories.(ts|tsx)",
+    "src/stories/**/*.(ts|tsx)",
+    "./*.ts",
+  ],
 }
 
 /** @type {import("rollup-plugin-postcss").PostCSSPluginConf} */
