873cc51e45861bf8ee12ab75e83cd4346e4cab65
Milind Ganjoo <milind.ganjoo@gmail.com>
2022-01-12 09:34:34 -0800

    Finish drag-based tests

--- tests/drag-moves.spec.ts
+++ tests/drag-moves.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "@playwright/test"
-import { expectHasPiece, expectMoveState } from "./helpers"
+import { expectHasPiece, expectMoveState, squareLocator } from "./helpers"
 
 test.beforeEach(async ({ page }) => {
   await page.goto("/")
@@ -20,13 +20,36 @@ test("drag-based moves work correctly", async ({ page }) => {
   await expect(page.locator("body")).toBeFocused()
 })
 
-test("keyboard-based move is ignored while drag is in progress", async () => {
-  test.fixme()
-})
-
 // If we click on a square, then mousedown (but not mouseup), then we should
 // still permit a drag operation to be completed instead of canceling move
 // which is the regular behavior.
-test("drag is completed even after previous click on square", async () => {
-  test.fixme()
+test.only("drag is completed even after previous click on square", async ({
+  page,
+}) => {
+  // click on first square once
+  await squareLocator(page, "f2").click()
+
+  // get location of f2 and e6 square
+  const fromRect = await squareLocator(page, "f2").boundingBox()
+  const toRect = await squareLocator(page, "e6").boundingBox()
+
+  // mousedown on f2 again but this time move to e6
+  if (fromRect !== null) {
+    await page.mouse.move(
+      fromRect.x + fromRect.width / 2,
+      fromRect.y + fromRect.height / 2
+    )
+  }
+  await page.mouse.down()
+  if (toRect !== null) {
+    await page.mouse.move(
+      toRect.x + toRect.width / 2,
+      toRect.y + toRect.height / 2
+    )
+  }
+  await page.mouse.up()
+
+  // second square should be marked with a piece on it, and we should be back to awaiting input
+  await expectHasPiece(page, "e6", true)
+  await expectMoveState(page, "awaiting-input")
 })
