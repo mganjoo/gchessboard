a11ceb38e1b81c1557fb35758ec1baa01e4fd05c
Milind Ganjoo <milind.ganjoo@gmail.com>
2022-02-07 19:58:14 -0800

    Add additional browsers to Playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,24 @@
-import { PlaywrightTestConfig } from "@playwright/test";
+import { PlaywrightTestConfig, devices } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+  ],
   testDir: "tests",
   forbidOnly: !!process.env.CI,
   retries: 1,
-  workers: process.env.CI ? 2 : undefined,
+  workers: process.env.CI ? 3 : undefined,
   use: {
     baseURL: "http://localhost:3000",
     trace: "on-first-retry",
